06db6de4740a08ce75564c63b5db5fcbd4527803
MohamedElsoudy <mohamedelsoudy373@gmail.com>
2025-05-01 22:58:44 +0300

    add silder

--- src/app/feature/modules/public/components/hero/hero.component.ts
+++ src/app/feature/modules/public/components/hero/hero.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewEncapsulation } from '@angular/core';
+interface Slide {
+  title: string;
+  description: string;
+  features: string[];
+}
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrl: './hero.component.scss',
+  encapsulation: ViewEncapsulation.None
 })
 export class HeroComponent {
-  title = 'World Cup Event';
-  description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Soluta, molestias repudiandae pariatur.';
+  slides: Slide[] = [
+    {
+      title: 'TalentMap - Révélateur de Potentiels',
+      description: 'Application innovante développée par AFRISPORT Dynamics pour révolutionner la détection et le suivi des talents sportifs africains.',
+      features: ['Détection', 'Suivi progression', 'Valorisation', 'Mise en relation', 'Analytique']
+    },
+    {
+      title: 'Foot4Impact - Le Sport au Service du Changement',
+      description: 'Application révolutionnaire développée par AFRISPORT Dynamics pour maximiser et valoriser l\'impact social du football.',
+      features: ['Identification des besoins', 'Mise en relation', 'Pilotage de projets', 'Mesure d\'impact']
+    }
+  ];
+
+
 }
